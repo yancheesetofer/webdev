ad2b92cbe49184435ebb56b3dfba41bcd30efc45
yancheesetofer <yan.christofer@ui.ac.id>
2023-02-27 03:17:26 +0700

    Add Hero section component

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,19 +2,26 @@ import Head from 'next/head'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 import Header from '@/components/Header';
+import Hero from '@/components/Hero';
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <>
+    <div className='bg-[rgb(36,36,36)] text-white 
+    h-screen snap-y snap-mandatory overflow-scroll z-0'>
       <Head>
         <title>Yancheesetofer&apos Portfolio</title>
       </Head>
-      {/*Header */}
+      
       <Header />
       
       {/* Hero */}
+
+      <section id="hero" className='snap-center'>
+        <Hero />
+
+      </section>
       
       {/* About */}
       
@@ -25,6 +32,6 @@ export default function Home() {
       {/* Projects */}
       
       {/* Contact Me */}
-    </>
+    </div>
   )
 }
