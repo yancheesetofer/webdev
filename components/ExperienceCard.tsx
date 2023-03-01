01124632ef17ac75396124449e5ae21e659b0ee8
yancheesetofer <yan.christofer@ui.ac.id>
2023-03-02 00:14:22 +0700

    Implement motion and transition for Experience Cards.

--- components/ExperienceCard.tsx
+++ components/ExperienceCard.tsx
@@ -5,7 +5,9 @@ type Props = {}
 
 function ExperienceCard({}: Props) {
   return (
-    <article>
+    <article className='flex flex-col rounded-lg items-center space-y-7 flex-shrink-0
+    w-[500px] md:w-[600px] xl:w-[900px] snap-center bg-[#292929] p-10 hover:opacity-100
+    opacity-40 cursor-pointer transition-opacity duration-100 overflow-hidden'>
       <motion.img
         initial={{
           y: -100,
@@ -13,6 +15,7 @@ function ExperienceCard({}: Props) {
         }}
         transition={{ duration: 1.2 }}
         whileInView={{ opacity: 1, y: 0 }}
+        viewport={{once: true}}
         className="w-32 h-32 rounded-full xl:w-[200px] xl:h-[200px] object-cover
       object-center"
         src="https://cdn.techinasia.com/data/images/jJf2w5xYGrbAC89aZd1SLhLFUOfSmDzHqYDK80ef.png"
@@ -32,12 +35,12 @@ function ExperienceCard({}: Props) {
           {/* Tech used */}
           {/* Tech used */}
         </div>
-        <p>Started work... - Ended ...</p>
+        <p className='uppercase py-5 text-gray-300'>Started work... - Ended ...</p>
         <ul className="list-disc space-y-4 ml-5 text-lg">
-          <li>summary points</li>
-          <li>summary points</li>
-          <li>summary points</li>
-          <li>summary points</li>
+          <li>summary pointssummary pointssummary pointssummary pointssummary pointssummary pointssummary pointssummary points</li>
+          <li>summary pointssummary pointssummary pointssummary pointssummary pointssummary pointssummary pointssummary points</li>
+          <li>summary pointssummary pointssummary pointssummary pointssummary pointssummary pointssummary pointssummary points</li>
+          <li>summary pointssummary pointssummary pointssummary pointssummary pointssummary pointssummary pointssummary points</li>
         </ul>
       </div>
     </article>
