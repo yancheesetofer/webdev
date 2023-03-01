17c21d38ab0043e5a631086e3ad80164936c9da4
yancheesetofer <yan.christofer@ui.ac.id>
2023-03-01 23:55:32 +0700

    Initiate Experience Card component

--- components/ExperienceCard.tsx
+++ components/ExperienceCard.tsx
@@ -7,11 +7,39 @@ function ExperienceCard({}: Props) {
   return (
     <article>
       <motion.img
-      className='w-32 h-32 rounded-full xl:w-[200px] xl:h-[200px] object-cover
-      object-center'
-        src="https://skystarventures.com/wp-content/uploads/2018/06/Agile-Technica.png"
+        initial={{
+          y: -100,
+          opacity: 0,
+        }}
+        transition={{ duration: 1.2 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        className="w-32 h-32 rounded-full xl:w-[200px] xl:h-[200px] object-cover
+      object-center"
+        src="https://cdn.techinasia.com/data/images/jJf2w5xYGrbAC89aZd1SLhLFUOfSmDzHqYDK80ef.png"
         alt="Agile Technica"
       />
+
+      <div className="px-0 md:px-10">
+        <h4 className="text-4xl font-light">Software Developer Intern</h4>
+        <p className="font-bold text-2xl mt-1">Agile Technica</p>
+        <div className="flex space-x-2 my-2">
+          {/* Tech used */}
+          <img
+            className="w-10 h-10 rounded-full"
+            src="https://www.soladrive.com/wp-content/uploads/2021/02/erpnext-logo.png"
+            alt="Erpnext"
+          />
+          {/* Tech used */}
+          {/* Tech used */}
+        </div>
+        <p>Started work... - Ended ...</p>
+        <ul className="list-disc space-y-4 ml-5 text-lg">
+          <li>summary points</li>
+          <li>summary points</li>
+          <li>summary points</li>
+          <li>summary points</li>
+        </ul>
+      </div>
     </article>
   );
 }
